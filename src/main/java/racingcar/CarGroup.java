6ca7d939fe2aa0e540b74892b4201cc41de812b3
김준형 <kmcc561@gmail.com>
2025-10-27 19:22:51 +0900

    우승자 계산 및 출력 구현

--- src/main/java/racingcar/CarGroup.java
+++ src/main/java/racingcar/CarGroup.java
@@ -1,7 +1,9 @@
 package racingcar;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
 -모든 자동차 관리 및 이동처리
@@ -25,5 +27,13 @@ public class CarGroup {
 
     public List<Car> all() {
         return cars;
+    }
+    //가장 멀리 간 자동차를 찾아서 우승자 리스트 반환
+    public List<String> getWinners() {
+        int max = cars.stream().mapToInt(Car::getDistance).max().orElse(0);
+        return cars.stream()
+                .filter(c -> c.getDistance() == max)
+                .map(Car::getName)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/racingcar/RaceView.java
+++ src/main/java/racingcar/RaceView.java
@@ -1,15 +1,22 @@
 package racingcar;
 
+import java.util.List;
+
 public class RaceView {
 
     public void showStartMessage() {
         System.out.println("\n실행 결과");
     }
+
     //현재 라운드 자동차 이동 결과 출력
     public void showRound(CarGroup group) {
         for (Car car : group.all()) {
             System.out.println(car.getName() + " : " + car.progress());
         }
         System.out.println();
+    }
+
+    public void showWinners(List<String> winners) {
+        System.out.println("최종 우승자: " + String.join(", ", winners));
     }
 }
